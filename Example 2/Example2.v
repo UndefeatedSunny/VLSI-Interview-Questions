dd97de58c55e90d715e602dd6fd1c8fbfad0218d
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-01 15:34:40 +0530

    Add files via upload

---
+++ Example 2/Example2.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+// Design Name: EXAMPLE (2) --->> Write Verilog code for a flip-flop with a negative-edge clock and asynchronous clear
+
+module Example2(Q, D, clk, clear);
+output Q;
+input D, clk, clear;       
+reg Q;
+always@(negedge clk or posedge clear)   // Change on negative edge always 
+
+begin
+if(clear)
+Q <= 1'b0;
+else
+Q <= D;   
+   
+end
+endmodule
