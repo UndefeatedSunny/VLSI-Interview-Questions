55576327535e5c0a421824dd29a5751322742659
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-05 22:47:58 +0530

    Add files via upload

---
+++ Example 3/Example3.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+// Design Name: EXAMPLE (3) --->> Write Verilog code for the flip-flop with a positive-edge clock and synchronous set
+
+module Example3(Q, D, clk, set);
+output Q;
+input D, clk, set;       
+reg Q;
+always@(posedge clk)   
+
+begin
+if(set)
+Q <= 1'b1;
+else
+Q <= D;   
+   
+end
+endmodule
+
