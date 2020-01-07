06688dec7bc17aa93899b86be36550afae8d1b43
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-07 19:30:42 +0530

    Add files via upload

---
+++ Example 4/Example4.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+// Design Name: EXAMPLE (4) --->>   Write Verilog code for the flip-flop with a positive-edge clock and clock enable.
+
+module Example4(Q, D, clk, enable);
+
+output Q;
+input D, clk, enable;       
+reg Q;
+always@(posedge clk)   
+
+begin
+if(enable)
+
+Q <= D;   
+   
+end
+endmodule
