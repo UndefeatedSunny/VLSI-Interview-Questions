acdbd4b4e37ce586cc75457f9d82525d5ac9af47
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-09 23:48:04 +0530

    Add files via upload

---
+++ Example 5/Example5.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+// Design Name: EXAMPLE (5) --->>   Write Verilog code for a 4-bit register with a positive-edge clock, asynchronous set and clock enable.
+
+module Example5(Q, D, clk, set, enable);
+output [3:0]Q;
+input [3:0]D; 
+input clk, set, enable;       
+reg [3:0]Q;
+always@(posedge clk or posedge set)   // Change on negative edge always 
+
+begin
+if(set==1'b1)
+Q <= 4'b1111;
+else if(enable==1'b1)
+Q <= D;   
+   
+end
+endmodule
