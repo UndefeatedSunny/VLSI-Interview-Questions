b3ebd6a37711270ca11861abd349cf1a8a279e41
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-14 00:05:44 +0530

    Add files via upload

---
+++ Example 11/Example11.v
@@ -0,0 +1,22 @@
+
+`timescale 1ns / 1ps
+
+// Design Name: EXAMPLE (11) --->>   Write Verilog code for a 4-bit unsigned up counter with asynchronous clear
+
+module Example11(counter, clk, clear);
+
+output [3:0]counter;
+input clk, clear;
+reg [3:0]counter_reg;
+
+always @(posedge clk or posedge clear)
+begin
+if(clear)
+counter_reg <= 4'b0000;
+else 
+counter_reg <= counter_reg + 4'b0001;
+end
+
+//                                               assign counter <= counter_reg;     -->> Line 20: Syntax error near "<=".                                       
+assign counter = counter_reg;
+endmodule
