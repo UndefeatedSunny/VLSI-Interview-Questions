2e74023460b9300c9dde92c0e7261fbb41902299
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-15 19:13:57 +0530

    Add files via upload

---
+++ Example 12/Example12.v
@@ -0,0 +1,22 @@
+
+`timescale 1ns / 1ps
+ 
+// Design Name: EXAMPLE (12) --->>   Write Verilog code for a 4-bit unsigned down counter with synchronous set 
+
+module Example12(counter, clk, set);
+
+output [3:0]counter;
+input clk, set;
+reg [3:0]counter_reg;
+
+always @(posedge clk)
+begin
+if(set)
+counter_reg <= 4'b1111;
+else 
+counter_reg <= counter_reg - 4'b0001;
+end
+
+assign counter = counter_reg;
+
+endmodule
