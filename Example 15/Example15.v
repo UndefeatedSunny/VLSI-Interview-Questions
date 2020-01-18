703e20d52e8e564afa12e8c0fc3f4f109985cf3f
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-18 22:29:45 +0530

    Add files via upload

---
+++ Example 15/Example15.v
@@ -0,0 +1,22 @@
+
+`timescale 1ns / 1ps
+ 
+// Design Name: EXAMPLE (15) --->>   Write Verilog code for a 4-bit unsigned up counter with an asynchronous clear and clock enable.
+
+module Example15(counter, clk, clear, enable);
+
+output [3:0]counter;
+input clk, clear, enable;
+reg [3:0]counter_reg;
+
+always @(posedge clk or posedge clear)
+begin
+if(clear)
+counter_reg <= 4'b0000;
+else if(enable) 
+counter_reg <= counter_reg + 4'b0001;
+end
+
+assign counter = counter_reg;
+
+endmodule
