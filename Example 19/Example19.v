8b9e93c578ec0800951da2434c45d69bac27ac5d
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-19 22:26:29 +0530

    Add files via upload

---
+++ Example 19/Example19.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+ 
+// Design Name: EXAMPLE (19) --->>   Write Verilog code for a 4-bit unsigned up accumulator with an asynchronous clear.
+/*
+Difference between counter and accumulator
+
+Counter = count +1
+Accumulator = count (+or-) any variable or constant.
+*/
+module Example19(counter, sunny, clk, clear);
+
+output [3:0]counter;
+input [3:0]sunny;
+input clk, clear;
+reg [3:0]counter_reg;
+
+always @(posedge clk or posedge clear)
+begin
+if(clear)
+counter_reg <= 4'b0000;
+else 
+counter_reg <= counter_reg + sunny;
+end
+                                     
+assign counter = counter_reg;
+endmodule
