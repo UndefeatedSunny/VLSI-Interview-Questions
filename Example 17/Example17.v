d4c1c4767a360f7daa42e2c6eb2b5cd6c0ba4663
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-01-30 00:15:23 +0530

    Add files via upload

---
+++ Example 17/Example17.v
@@ -0,0 +1,22 @@
+
+`timescale 1ns / 1ps
+ 
+// Design Name: EXAMPLE (17) --->>   Write Verilog code for a 4-bit signed up counter with an asynchronous reset.
+
+module Example17(counter, clk, reset);
+
+output signed [4:0]counter;
+input clk, reset;
+reg signed [4:0]counter_reg;
+
+always @(posedge clk or posedge reset)
+begin
+if(reset)
+counter_reg <= 5'b00000;
+else 
+counter_reg <= counter_reg + 5'b00001;
+end
+
+assign counter = counter_reg;
+
+endmodule
